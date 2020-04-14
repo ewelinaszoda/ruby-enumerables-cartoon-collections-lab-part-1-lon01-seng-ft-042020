acdecc25a746bd7a216587a7bf1701f02d7ebb23
Ewelina Szoda <ewelinaszoda@gmail.com>
2020-04-14 12:13:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,8 @@
-require 'pry'  
+ 
 def greet_characters(array)
   # Use `each` to enumerate over the provided array
   # Print a custom greeting for each element
   array.each {|element| puts "Hello #{element}!"}
-  binding.pry
 end
 greet_characters(array)
 
